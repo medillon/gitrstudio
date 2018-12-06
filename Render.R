6e37e0a5f79667365e337cdabe0f20330f681d52
Michael Dillon <Michael.Dillon@uwyo.edu>
2018-12-06 16:13:20 -0700

    R rendering practice

---
+++ Render.R
@@ -0,0 +1,15 @@
+# ---
+# title: "New R Markdown version control"
+# author: "Michael Dillon"
+# date: "December 6, 2018"
+# output: github_document
+# ---
+
+#' This is my very own R markdown document
+# 
+
+plot(1:10)
+text(2, 6, "yes")
+
+
+ 
